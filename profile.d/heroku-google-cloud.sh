224bc7ea7ac68a2d4739dfd261d73c3c01cce768
Jimmy Couch <jimmy@terminuslabs.io>
2021-12-14 14:22:04 -0600

    Add setting of kubectl credentials based on GKE cluster

--- profile.d/heroku-google-cloud.sh
+++ profile.d/heroku-google-cloud.sh
@@ -3,19 +3,19 @@
 # Add gcloud to PATH
 export PATH=/app/vendor/google-cloud-sdk/bin:$PATH
 
-if [ -z "$GOOGLE_CREDENTIALS" ]; then
-    echo "GOOGLE_CREDENTIALS not set"
-else
-    export GOOGLE_APPLICATION_CREDENTIALS='/app/google-credentials.json'
-    echo "$GOOGLE_CREDENTIALS" | base64 -d > $GOOGLE_APPLICATION_CREDENTIALS
-    gcloud auth activate-service-account --key-file /app/google-credentials.json
-    gcloud container clusters get-credentials autopilot-cluster-1
-fi
+#if [ -z "$GOOGLE_CREDENTIALS" ]; then
+#    echo "GOOGLE_CREDENTIALS not set"
+#else
+#    export GOOGLE_APPLICATION_CREDENTIALS='/app/google-credentials.json'
+#    echo "$GOOGLE_CREDENTIALS" | base64 -d > $GOOGLE_APPLICATION_CREDENTIALS
+#    gcloud auth activate-service-account --key-file /app/google-credentials.json
+#    gcloud container clusters get-credentials autopilot-cluster-1
+#fi
 
 # Set default project and compute zone
-if [ ! -z $PROJECT ]; then
-    gcloud config set project $PROJECT
-fi
-if [ ! -z $ZONE ]; then
-    gcloud config set compute/zone $ZONE
-fi
+#if [ ! -z $PROJECT ]; then
+#    gcloud config set project $PROJECT
+#fi
+#if [ ! -z $ZONE ]; then
+#    gcloud config set compute/zone $ZONE
+#fi
